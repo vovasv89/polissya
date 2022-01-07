c78452f0c6998b11770fda34bf2338d123bf6841
vovasv89 <97290918+vovasv89@users.noreply.github.com>
2022-01-07 16:07:14 +0200

    Initial commit

---
+++ Корисні посилання.php
@@ -0,0 +1,22 @@
+<?php
+
+include("Полісся.php");
+
+?>
+
+<style>
+		body {
+background-image: url("img/Polissia-lis-richka-zemlia.jpg.jpg");
+background-size: cover;
+}
+	</style>
+<div class="row">  
+	<p> https://www.botany.kiev.ua/doc/FitodiversityUA_Polissia_2006.pdf</p>
+
+<p> https://scholar.google.com.ua/citations?view_op=view_citation&hl=uk&user=tuULq4sAAAAJ&alert_preview_top_rm=2&citation_for_view=tuULq4sAAAAJ:Tiz5es2fbqcC</p>
+
+<p> https://redbook-ua.org</p>
+
+</div>
+</body>
+</html>
